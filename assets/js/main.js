41a231743bafdc1452fd54965fa3ce18ff714a30
samy <sam.younger@gmail.com>
2018-10-25 20:39:40 +0100

    image js selector

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
-// SCROLL ACTION TO ANCHOR TAG
-$(document).on('click', 'a[href^="#"]', function (event) {
+document.addEventListener("DOMContentLoaded", function(event){
+  addScrollEvent();
+
+  var images = document.getElementsByClassName("thumbnail");
+  addListenerToImages(images);
+});
+
+function changeSelectedImage(event) {
+  event.preventDefault();
+  imageSrc = event.currentTarget.src;
+  var selectedImage = document.getElementById("selected-image");
+  selectedImage.src = imageSrc;
+}
+
+function addListenerToImages(images) {
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', changeSelectedImage, false);
+  }
+}
+
+function addScrollEvent() {
+  $('a[href^="#"]').on('click', function(event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
-});
+  });
+}
